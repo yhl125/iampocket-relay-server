466114e74d933545823ea3ac218b4a2f081adead
Yoonhyoung-Lee <54613623+Yoonhyoung-Lee@users.noreply.github.com>
2024-08-31 20:38:29 +0900

    feat: add swaggar

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -7,6 +8,14 @@ async function bootstrap() {
     origin: '*',
     methods: 'GET,POST',
   });
+  const config = new DocumentBuilder()
+    .setTitle('iampocket-relay-server')
+    .setDescription('Server for lit protocol for Telegram authentication and payments')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(3001);
 }
 bootstrap();
